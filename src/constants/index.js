c750e703ddcfe424c21065c6cba5e6321d64fd78
leokazuyukinagatani <40251968+leokazuyukinagatani@users.noreply.github.com>
2025-01-13 05:03:47 +0000

    refator: alterando projeto

--- src/constants/index.js
+++ src/constants/index.js
@@ -51,11 +51,11 @@ export const clientReviews = [
 
 export const myProjects = [
   {
-    title: 'Podcastr - AI Podcast Platform',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    title: 'Zigen',
+    desc: 'Landing page de aplicativo de musíca',
     subdesc:
-      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+      'Contruindo uma página de vendas com HTML e CSS, para divulgar Zigen um aplicativo perfeito para aprender a cantar',
+    href: 'https://leokazuyukinagatani.github.io/zingen',
     texture: '/textures/project/project1.mp4',
     logo: '/assets/project-logo1.png',
     logoStyle: {
@@ -67,24 +67,14 @@ export const myProjects = [
     tags: [
       {
         id: 1,
-        name: 'React.js',
+        name: 'HTML',
         path: '/assets/react.svg',
       },
       {
         id: 2,
-        name: 'TailwindCSS',
+        name: 'CSS',
         path: 'assets/tailwindcss.png',
-      },
-      {
-        id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
+      }
     ],
   },
   {
